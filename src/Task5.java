e6ae0b834489856187a380abd8aea671e2f6d639
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-08-02 20:21:15 +0300

    Initial commit

---
+++ src/Task5.java
@@ -0,0 +1,6 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int q = 21, w = 8;
+        System.out.println(q + "/" + w + "=" + (q/w) + " и в остатке " + (q%w));
+    }
+}
