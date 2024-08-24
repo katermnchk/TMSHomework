886130fdc1d2bb6146aa67dc5dea2a68784296ae
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-08-24 15:02:38 +0300

    lesson6

--- src/lesson1/Task5.java
+++
@@ -1,8 +0,0 @@
-package lesson1;
-
-public class Task5 {
-    public static void main(String[] args) {
-        int q = 21, w = 8;
-        System.out.println(q + "/" + w + "=" + (q/w) + " и в остатке " + (q%w));
-    }
-}
