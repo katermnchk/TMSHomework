2a2e28b1b397e8b5c54764d6ff8f4a125cb33a3f
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-09-13 15:43:11 +0300

    with special task

--- src/MyMain.java
+++ src/MyMain.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public class MyMain {
     public static void main(String[] args) {
-        checkAbbreviation();
+        //checkAbbreviation();
+        checkDocument();
     }
 
     static void checkAbbreviation() {
@@ -15,6 +16,26 @@ public class MyMain {
         Matcher matcher = pattern.matcher(input);
         while(matcher.find())
             System.out.println(matcher.group());
+    }
+
+    static void checkDocument(){
+        System.out.print("Please enter a text: ");
+        String document = getScanner();
+        String regexNum = "\\b(?:\\d{4}-){2}\\d{2}\\b";
+        Pattern patternNum = Pattern.compile(regexNum);
+        Matcher matcherNum = patternNum.matcher(document);
+        while(matcherNum.find())
+            System.out.println("Number of document: " + matcherNum.group());
+        String regexPhone = "\\+\\(\\d{2}\\)\\d{7}";
+        Pattern patternPhone = Pattern.compile(regexPhone);
+        Matcher matcherPhone = patternPhone.matcher(document);
+        while(matcherPhone.find())
+            System.out.println("Phone: " + matcherPhone.group());
+        String regexEmail = "[A-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+        Pattern patternEmail = Pattern.compile(regexEmail);
+        Matcher matcherEmail = patternEmail.matcher(document);
+        while(matcherEmail.find())
+            System.out.println("Email: " + matcherEmail.group());
     }
 
     public static String getScanner(){
