bac31c2fcff3bd807d3b0816b358a0ea9b0b306e
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-09-24 16:02:31 +0300

    nothing

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,48 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class MyMain {
-    public static void main(String[] args) {
-        registration();
+    public static void main(String[] args)  throws IOException {
+        getFileReader();
+
     }
 
-    static void registration() {
-        System.out.print("Please enter your login: ");
-        String login = getScanner();
-        System.out.print("Please enter your password: ");
-        String password = getScanner();
-        System.out.print("Please confirm your password: ");
-        String confirmPassword = getScanner();
+    public static void getFileReader() {
+        try (FileReader read = new FileReader("C:/Users/katar/OneDrive/Рабочий стол/Romeo and Julietta (en).txt")){
+            int value;
+            int[] = int*Object words = null;
+           words;
 
-        try {
-            boolean isValid = validateCredentials(login, password, confirmPassword);
-            System.out.println("Verification successful: " + isValid);
-        } catch (WrongLoginException e) {
-            System.out.println("Login error: " + e.getMessage());
-        } catch (WrongPasswordException e) {
-            System.out.println("Password error: " + e.getMessage());
+            while((value = read.read()) != -1) {
+                //System.out.print((char) value);
+            }
+        } catch (IOException e) {
+            System.out.println("We did not manage to read the file...");
         }
     }
 
-    public static boolean validateCredentials(String login, String password, String confirmPassword)
-            throws WrongLoginException, WrongPasswordException {
 
-        if (login.length() >= 20 || login.contains(" ")) {
-            throw new WrongLoginException("Login must be less than 20 characters and contain no spaces");
-        }
-
-        if (password.length() >= 20 || password.contains(" ") || !password.matches(".*\\d.*")) {
-            throw new WrongPasswordException
-                    ("The password must be less than 20 characters, contain no spaces, and contain at least one number");
-        }
-
-        if (!password.equals(confirmPassword)) {
-            throw new WrongPasswordException("Passwords aren't excepted");
-        }
-        return true;
-    }
-
-    static String getScanner() {
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
-    }
 }
--- src/WrongLoginException.java
+++
@@ -1,9 +0,0 @@
-public class WrongLoginException extends Exception{
-    public WrongLoginException() {
-        super("Wrong Login");
-    }
-
-    public WrongLoginException(String message) {
-        super(message);
-    }
-}
