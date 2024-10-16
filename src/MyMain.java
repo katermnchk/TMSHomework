13284c96121711f5072cce5e17f8d6978cada814
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-10-16 16:37:06 +0300

    lesson17

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,97 +1,82 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 
 public class MyMain {
     public static void main(String[] args) {
         //getTask1();
         //getTask2();
-        getSpecialTask();
+        //getTask3();
+        //getTask4();
+        getTask5();
     }
 
     static void getTask1() {
-        String[] input1 = {"a", "b", "a", "c", "b"};
-        String[] input2 = {"c", "b", "a"};
-        String[] input3 = {"c", "c", "c", "c"};
-        System.out.println(wordMultiple(input1));
-        System.out.println(wordMultiple(input2));
-        System.out.println(wordMultiple(input3));
-    }
+        System.out.print("PLease enter the date on format yyyy-MM-dd: ");
+        String dateOfBirth = getScanner();
+        try {
+            LocalDate birthday = LocalDate.parse(dateOfBirth);
+            System.out.println("You will be 100 age on " + birthday.plusYears(100));
 
-    public static Map<String, Boolean> wordMultiple(String[] words) {
-        Map<String, Integer> myMap = new HashMap<>();
-        Map<String, Boolean> resultMap = new HashMap<>();
-        for (String word : words) {
-            myMap.put(word, myMap.getOrDefault(word, 0) + 1);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error");
         }
-
-        for (String word : myMap.keySet()) {
-            resultMap.put(word, myMap.get(word) >= 2);
-        }
-        return resultMap;
     }
 
     static void getTask2() {
-        String[] input1 = {"code", "bug"};
-        String[] input2 = {"man", "moon", "main"};
-        String[] input3 = {"man", "moon", "good", "night"};
-        System.out.println(pairs(input1));
-        System.out.println(pairs(input2));
-        System.out.println(pairs(input3));
-    }
-
-    public static Map<String, String> pairs(String[] words) {
-        Map<String, String> resultMap = new HashMap<>();
-        for (String word : words) {
-            String firstSymbol = String.valueOf(word.charAt(0));
-            String lastSymbol = String.valueOf(word.charAt(word.length()-1));
-            resultMap.put(firstSymbol, lastSymbol);
-        }
-        return resultMap;
-    }
-
-    static void getSpecialTask() {
-        String[] test = {"()",
-                "[()]",
-                "{[()]}",
-                "([{{[(())]}}])",
-                "{{[]()}}}}",
-                "{[(])}"};
-        for (String testing : test) {
-            if (checkForBalance(testing)) {
-                System.out.println(testing + " is balanced");
-            }
-            else System.out.println(testing + " is not balanced");
-        }
-    }
-
-    static boolean checkForBalance(String str) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char n : str.toCharArray()) {
-            if (n == '(' || n == '{' || n == '[') {
-                stack.push(n);
-            }
-            else if (n == ')' || n == '}' || n == ']') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (!isPair(stack.pop(), n)) {
-                    return false;
-                }
+        List<Integer> numbers = Arrays.asList(1, 2, 5, -4, 0, -8, -2);
+        List<Integer> positiveNumbers = new ArrayList<>();
+        Predicate<Integer> isPositive = x -> x > 0;
+        for (int num : numbers) {
+            if (isPositive.test(num)) {
+               positiveNumbers.add(num);
             }
         }
-        return stack.isEmpty();
+        System.out.println(positiveNumbers);
     }
 
-    static boolean isPair(char open, char close) {
-        return (open == '(' && close == ')') ||
-                (open == '{' && close == '}') ||
-                (open == '[' && close == ']');
+    static void getTask3() {
+        System.out.print("Please enter amount of money in BYN: ");
+        String amountInBYN = getScanner();
+        Function<String, Double> convertToDollars = x -> {
+            String[] parts = x.split(" ");
+            double byn = Double.parseDouble(parts[0]);
+            return byn/3.2;
+        };
+
+        double amountInUSD = convertToDollars.apply(amountInBYN);
+        System.out.println("Сумма в долларах: " + amountInUSD);
+    }
+
+    public static void getTask4(){
+        System.out.print("Please enter amount of money in BYN: ");
+        String amountInBYN = getScanner();
+        Consumer<String> convertToDollars = x -> {
+            String[] parts = x.split(" ");
+            double byn = Double.parseDouble(parts[0]);
+            System.out.println(byn/3.2);
+        };
+
+        convertToDollars.accept(amountInBYN);
+    }
+
+    public static void getTask5() {
+        Supplier<String> supplier = () -> {
+            System.out.print("Please enter a string: ");
+            String newString = getScanner();
+            return "Reversed string: " + new StringBuilder(newString).reverse().toString();
+        };
+        System.out.println(supplier.get());
     }
 
     public static String getScanner(){
         return new Scanner(System.in).nextLine();
     }
+
+
 }
