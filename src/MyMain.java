4d0cbb6db0b2d1b6f3bb8a176c7ea7404caf496b
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2024-10-21 15:19:54 +0300

    lesson18

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,82 +1,42 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 
 public class MyMain {
     public static void main(String[] args) {
         //getTask1();
-        //getTask2();
-        //getTask3();
-        //getTask4();
-        getTask5();
+        getTask2();
+
     }
 
     static void getTask1() {
-        System.out.print("PLease enter the date on format yyyy-MM-dd: ");
-        String dateOfBirth = getScanner();
-        try {
-            LocalDate birthday = LocalDate.parse(dateOfBirth);
-            System.out.println("You will be 100 age on " + birthday.plusYears(100));
-
-        } catch (DateTimeParseException e) {
-            System.out.println("Error");
-        }
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 1, 2, 5, 6, 6, 14, 14, 14, 35, 36, 37, 37));
+        int res = numbers.stream().distinct().filter(n -> n %2 == 0).mapToInt(Integer::intValue).sum();
+        System.out.println(numbers);
+        System.out.println(numbers.stream().distinct().toList());
+        System.out.println(numbers.stream().distinct().filter(n -> n %2 == 0).toList());
+        System.out.println("Sum = " + res);
     }
 
     static void getTask2() {
-        List<Integer> numbers = Arrays.asList(1, 2, 5, -4, 0, -8, -2);
-        List<Integer> positiveNumbers = new ArrayList<>();
-        Predicate<Integer> isPositive = x -> x > 0;
-        for (int num : numbers) {
-            if (isPositive.test(num)) {
-               positiveNumbers.add(num);
-            }
-        }
-        System.out.println(positiveNumbers);
+        Map<Integer, String> idName = new HashMap<>();
+        idName.put(1, "Katya");
+        idName.put(2, "Senya");
+        idName.put(3, "Ilya");
+        idName.put(5, "Anya");
+        idName.put(7, "Nastya");
+        idName.put(9, "Alesya");
+        idName.put(13, "Ivan");
+
+        List<String> names = idName.entrySet().stream().filter(n -> Arrays.asList(1,2,5,8,13).contains(n.getKey())).
+                map(Map.Entry::getValue).filter(n -> n.length() % 2 != 0).
+                map(n -> new StringBuilder(n).reverse().toString()).toList();
+        System.out.println(idName);
+        System.out.println(idName.entrySet().stream().filter(n -> Arrays.asList(1,2,5,8,13).
+                contains(n.getKey())).toList());
+        System.out.println(idName.entrySet().stream().filter(n -> Arrays.asList(1,2,5,8,13).contains(n.getKey())).
+                map(Map.Entry::getValue).filter(n -> n.length() % 2 != 0).toList());
+        System.out.println("\nResult: " + names);
     }
-
-    static void getTask3() {
-        System.out.print("Please enter amount of money in BYN: ");
-        String amountInBYN = getScanner();
-        Function<String, Double> convertToDollars = x -> {
-            String[] parts = x.split(" ");
-            double byn = Double.parseDouble(parts[0]);
-            return byn/3.2;
-        };
-
-        double amountInUSD = convertToDollars.apply(amountInBYN);
-        System.out.println("Сумма в долларах: " + amountInUSD);
-    }
-
-    public static void getTask4(){
-        System.out.print("Please enter amount of money in BYN: ");
-        String amountInBYN = getScanner();
-        Consumer<String> convertToDollars = x -> {
-            String[] parts = x.split(" ");
-            double byn = Double.parseDouble(parts[0]);
-            System.out.println(byn/3.2);
-        };
-
-        convertToDollars.accept(amountInBYN);
-    }
-
-    public static void getTask5() {
-        Supplier<String> supplier = () -> {
-            System.out.print("Please enter a string: ");
-            String newString = getScanner();
-            return "Reversed string: " + new StringBuilder(newString).reverse().toString();
-        };
-        System.out.println(supplier.get());
-    }
-
-    public static String getScanner(){
-        return new Scanner(System.in).nextLine();
-    }
-
 
 }
